9f7aa2e07a081a9b105884ebe2c7858de06ce76e
Ayoub11th <61156953+Ayoub11th@users.noreply.github.com>
2021-10-15 15:17:38 +0200

    cart resolvers

--- src/apollo/resolvers/cart.resolver.js
+++ src/apollo/resolvers/cart.resolver.js
@@ -1,1 +1,28 @@
-//TODO
+const Cart = require ('../schemas/cart.schema');
+
+module.exports = {
+    Query : {
+        Carts : () => {
+            return Cart.find()
+            .populate('products').populate('user');
+        },
+
+        Cart: (parent, args) => {
+            console.log("Get by id :", args.id);
+            return Cart.findById(args.id);
+        }
+    },
+    Mutation : {
+        createCart : (parent, args) => {
+            const newCart = new Cart ({
+                amountTotal: args.amountTotal,
+                user : args.user,
+                products: args.products,
+            });
+            return newCart.save();
+        },
+        deleteCart: (parent, args) => {
+            return Cart.findByIdAndDelete(args.id).catch((err)=>console.log(err));
+        }
+    },
+}
