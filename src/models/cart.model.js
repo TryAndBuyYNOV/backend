12f2fb4269237c5d5fb21b37f36b23595cf315cb
do-david <50655142+do-david@users.noreply.github.com>
2021-10-15 12:33:26 +0200

    feat: add .env in gitignore and create cart schema

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,1 +1,16 @@
-//TODO
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cartSchema = new Schema({
+    totalAmount: Number,
+    user: {
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+    products : [{
+        type: Schema.Types.ObjectId, ref: 'Product'
+    }]
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Cart', cartSchema);
