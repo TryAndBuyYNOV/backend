a2d7a154bfe03453dc88296fbd99810a3aec7dcf
do-david <50655142+do-david@users.noreply.github.com>
2021-10-15 12:22:15 +0200

    feat: create cart model

--- src/apollo/schemas/cart.schema.js
+++ src/apollo/schemas/cart.schema.js
@@ -1,1 +1,19 @@
-//TODO
+const {gql} = require('apollo-server-express');
+const User = require('./user.schema');
+
+module.exports = gql`
+type Cart {
+    id: ID!
+    totalAmount: Number!
+    user: User
+  }
+  extend type Query {
+    products: [Product]
+    product(id: ID!): Product
+  }
+  extend type Mutation {
+    createProduct(title: String!, priceHT: Float!, description: String!,category: String!): Product
+    updateProduct(id:ID!,title: String!, priceHT: Float!, description: String!,category: String!): Product
+    deleteProduct(id:ID!): Product
+  }
+`;
